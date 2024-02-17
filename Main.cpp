634ec0b1aaaf394242c1444d743d203b8ce8a525
leviCP <145071985+leviCP@users.noreply.github.com>
2024-02-17 13:54:05 -0800

    Add files via upload
    nested if statements for ingredient.isempty()

---
+++ Main.cpp
@@ -0,0 +1,232 @@
+#include "Header.hpp"
+#include "Functions.hpp"
+
+int main(void)
+{
+
+
+	if (dairy.isempty() == true)
+	{	
+		if (produce.isempty() == true)
+		{
+			if (dry.isempty() == true)
+			{
+				if (protien.isempty() == true)
+				{
+					//no ingrdients where entered
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (protien.isempty() == true)
+			{
+				if (dry.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (dry.isempty() == true)
+		{
+			if (produce.isempty() == true)
+			{
+				if (protien.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (protien.isempty() == true)
+			{
+				if (produce.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (protien.isempty() == true)
+		{
+			if (produce.isempty() == true)
+			{
+				if (dry.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (dry.isempty() == true)
+			{
+				if (produce.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+	}
+
+	if (produce.isempty() == true)
+	{
+		if (dairy.isempty() == true)
+		{
+			if (protein.isempty() == true)
+			{
+				if (dry.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (dry.isempty() == true)
+			{
+				if (protein.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (protein.isempty() == true)
+		{
+			if (dry.isempty() == true)
+			{
+				if (dairy.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (dairy.isempty() == true)
+			{
+				if (dry.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (dry.isempty() == true)
+		{
+			if (dairy.isempty() == true)
+			{
+				if (protien.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (protien.isempty() == true)
+			{
+				if (dairy.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+	}
+
+	if (dry.isempty() == true)
+	{
+		if (dairy.isempty() == true)
+		{
+			if (produce.isempty() == true)
+			{
+				if (protien.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (protien.isempty() == true)
+			{
+				if (produce.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (produce.isempty() == true)
+		{
+			if (dairy.isempty() == true)
+			{
+				if (protien.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+
+			}
+			if (protien.isempty() == true)
+			{
+				if (dairy.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (protien.isempty() == true)
+		{
+			if (dairy.isempty() == true)
+			{
+				if (produce.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (produce.isempty() == true)
+			{
+				if (dairy.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+	}
+
+	if (protien.isempty() == true)
+	{
+		if (dairy.isempty() == true)
+		{
+			if (produce.isempty() == true)
+			{
+				if (dry.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (dry.isempty() == true)
+			{
+				if (produce.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (produce.isempty() == true)
+		{
+			if (dairy.isempty() == true)
+			{
+				if (dry.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (dry.isempty() == true)
+			{
+				if (dairy.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+		if (dry.isempty() == true)
+		{
+			if (dairy.isempty() == true)
+			{
+				if (produce.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+			if (produce.isempty() == true)
+			{
+				if (dairy.isempty() == true)
+				{
+					printf("all ingredient lists were empty");
+				}
+			}
+		}
+	}
+
+
+	return 0;
+}
