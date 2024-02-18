dbb26e9d8eb67daf455fbdc49797ad11473aaba8
leviCP <145071985+leviCP@users.noreply.github.com>
2024-02-18 02:09:06 -0800

    Add files via upload
    more buttons to button

---
+++ mrebuttonscpp.cpp
@@ -0,0 +1,177 @@
+if (potatoes.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	produce.populate(potatoes.returnString());
+
+
+}
+
+if (lettuce.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	produce.populate(lettuce.returnString());
+
+
+}
+
+if (tomato.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+	
+	produce.populate(tomato.returnString());
+
+
+}
+
+if (onions.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	produce.populate(onions.returnString());
+
+
+}
+
+if (greenBeans.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	produce.populate(greenBeans.returnString());
+
+
+}
+
+if (garlicCloves.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	produce.populate(garlicCloves.returnString());
+
+
+}
+
+if (mushrooms.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	produce.populate(mushrooms.returnString());
+
+
+}
+
+if (noodles.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	dry.populate(noodles.returnString());
+
+
+}
+
+if (rice.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	dry.populate(rice.returnString());
+
+
+}
+
+if (bread.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	dry.populate(bread.returnString());
+
+
+}
+
+if (buns.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	dry.populate(buns.returnString());
+
+
+}
+
+if (nuts.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	dry.populate(nuts.returnString());
+
+
+}
+
+if (flour.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	dry.populate(flour.returnString());
+
+
+}
+
+if (cereal.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	dry.populate(cereal.returnString());
+
+
+}
+
+/////////////////////////////////////PROTEIN
+
+if (beef.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(beef.returnString());
+
+
+}
+
+if (chicken.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(chicken.returnString());
+
+
+}
+
+if (beans.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(beans.returnString());
+
+
+}
+
+if (fish.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(fish.returnString());
+
+
+}
+
+if (pork.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(pork.returnString());
+
+
+}
+
+if (bacon.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(bacon.returnString());
+
+
+}
+
+if (lunchmeat.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(lunchmeat.returnString());
+
+
+}
+
+if (tofu.returningGlobalBounds().contains(sf::Mouse::getPosition(window).x,
+	sf::Mouse::getPosition(window).y)) {
+
+	protien.populate(tofu.returnString());
+
+
+}
