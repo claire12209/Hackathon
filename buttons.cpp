af1ae2dfe5bede33269ec212d6dcdf90851e1312
leviCP <145071985+leviCP@users.noreply.github.com>
2024-02-18 01:30:05 -0800

    Add files via upload
    buttons coded w/ stuff
    i have been awake for 18 hours

---
+++ buttons.cpp
@@ -0,0 +1,133 @@
+
+buttons potatoes;
+potatoes.setString("potatoes");
+sf::Text pot = potato.singlebutton(560, 305);
+pot.setFont(font);
+
+
+buttons lettuce;
+lettuce.setString("Lettuce");
+sf::Text let = lettuce.singlebutton(560, 380);
+let.setFont(font);
+
+
+buttons tomatos;
+tomatos.setString("tomatos");
+sf::Text tom = tomatos.singlebutton(560, 455);
+tom.setFont(font);
+
+
+buttons onions;
+onions.setString("onions");
+sf::Text oni = onions.singlebutton(560, 530);
+oni.setFont(font);
+
+
+buttons greenBeans;
+greenBeans.setString("green beans");
+sf::Text gB = greenBeans.singlebutton(560, 605);
+gB.setFont(font);
+
+
+buttons garlicCloves;
+garlicCloves.setString("Garlic Cloves");
+sf::Text gC = garlicCloves.singlebutton(560, 680);
+gC.setFont(font);
+
+
+buttons mushrooms;
+mushrooms.setString("mushrooms");
+sf::Text mush = mushrooms.singlebutton(560, 755);
+mush.setFont(font);
+
+///////////////////////////////////////////DRY
+
+buttons noodles;
+noodles.setString("noodles");
+sf::Text nood = noodles.singlebutton(760, 230);
+nood.setFont(font);
+
+
+buttons rice;
+rice.setString("rice");
+sf::Text ric = ric.singlebutton(760, 305);
+ric.setFont(font);
+
+
+buttons bread;
+bread.setString("bread");
+sf::Text bre = bread.singlebutton(760, 380);
+bre.setFont(font);
+
+
+buttons buns;
+buns.setString("buns");
+sf::Text bu = buns.singlebutton(760, 455);
+bu.setFont(font);
+
+
+buttons nuts;
+nuts.setString("nuts");
+sf::Text nut = nuts.singlebutton(760, 530);
+nut.setFont(font);
+
+
+buttons flour;
+flour.setString("flour");
+sf::Text flo = flour.singlebutton(760, 605);
+flo.setFont(font);
+
+
+buttons cereal;
+cereal.setString("cerael");
+sf::Text cer = cereal.singlebutton(760, 680);
+cer.setFont(font);
+
+//////////////////////////////////////////////////////PROTEIN
+
+buttons beef;
+beef.setString("beef");
+sf::Text bef = beef.singlebutton(960, 230);
+bef.setFont(font);
+
+
+buttons chicken;
+chicken.setString("chicken");
+sf::Text chk = chicken.singlebutton(960, 305);
+chk.setFont(font);
+
+
+buttons beans;
+beans.setString("beans");
+sf::Text bea = beans.singlebutton(960, 380);
+bea.setFont(font);
+
+
+buttons fish;
+fish.setString("fish");
+sf::Text fsh = fish.singlebutton(960, 455);
+fsh.setFont(font);
+
+
+buttons pork;
+pork.setString("pork");
+sf::Text prk = pork.singlebutton(960, 530);
+prk.setFont(font);
+
+
+buttons bacon;
+bacon.setString("bacon");
+sf::Text bac = bacon.singlebutton(960, 605);
+bac.setFont(font);
+
+
+buttons lunchmeat;
+lunchmeat.setString("lunchmeat");
+sf::Text lnchm = lunchmeat.singlebutton(960, 680);
+lnchm.setFont(font);
+
+
+buttons lofu;
+tofu.setString("tofu");
+sf::Text tfu = tofu.singlebutton(960, 755);
+tfu.setFont(font);
